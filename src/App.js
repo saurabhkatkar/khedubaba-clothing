a22d5cfc7e5b682015b7fe21e01e8ad9948e29c9
khedubaba <adityakhedekar49@gmail.com>
2021-12-26 19:57:36 +0530

    Router setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Route } from 'react-router';
 import './App.css';
-import Directory from './components/directory/directory.component';
 import { HomePage } from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+      <h1>HATS PAGE</h1>
+  </div>
+);
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
