335da7995921d0e4125f1c4afebc54cee467aebd
khedubaba <adityakhedekar49@gmail.com>
2021-12-27 19:05:19 +0530

    implemented firebase utils, included ability to store

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,36 @@ const config = {
     measurementId: "G-6DQPDF081E"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapshot = await userRef.get();
+
+    console.log(snapshot);
+
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        }
+        catch(error) {
+            console.log('error creating user ', error.message);
+
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
